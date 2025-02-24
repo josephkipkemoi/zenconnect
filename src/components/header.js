13edabbfec63280a2e32e5ab14bf3b37fdd59c24
josephkipkemoi <jkemboe@gmail.com>
2025-02-24 19:12:52 +0000

    Header links icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,7 @@
+import { faAddressBook, faContactBook, faMap } from "@fortawesome/free-regular-svg-icons"
+import { faBook, faHome, faKitMedical, faPeopleGroup, faPhone, faPhoneSquare, faSignHanging, faSquarePhone } from "@fortawesome/free-solid-svg-icons"
+import { faMobilePhone } from "@fortawesome/free-solid-svg-icons/faMobilePhone"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Container, Nav, Navbar } from "react-bootstrap"
 
 const HeaderComponent = () => {
@@ -12,13 +16,34 @@ const HeaderComponent = () => {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">About Us</Nav.Link>
-                            <Nav.Link href="#link">Features</Nav.Link>
-                            <Nav.Link href="#link">Therapists</Nav.Link>
-                            <Nav.Link href="#link">Resources</Nav.Link>
-                            <Nav.Link href="#link">Community</Nav.Link>
-                            <Nav.Link href="#link">Contact Us</Nav.Link>
+                            <Nav.Link href="#home" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faHome} />
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href="#link" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faMap} />
+                                About Us
+                            </Nav.Link>
+                            <Nav.Link href="#link" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faSignHanging} />
+                                Features
+                            </Nav.Link>
+                            <Nav.Link href="#link" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faKitMedical} />
+                                Therapists
+                            </Nav.Link>
+                            <Nav.Link href="#link" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faBook} />
+                                Resources
+                            </Nav.Link>
+                            <Nav.Link href="#link" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faPeopleGroup} />
+                                Community
+                            </Nav.Link>
+                            <Nav.Link href="#link" className="d-flex align-items-center">
+                                <FontAwesomeIcon className="m-1" icon={faSquarePhone} />
+                                Contact Us
+                            </Nav.Link>
                         </Nav>
                         </Navbar.Collapse>
                     </Container>
