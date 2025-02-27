506c9dfaa2844a5f05ee791acb1c7f0261318a78
josephkipkemoi <jkemboe@gmail.com>
2025-02-27 16:55:28 +0000

    handle loginForm component

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,28 +1,43 @@
+import { useState } from "react"
 import { Container, Form } from "react-bootstrap"
 
 const LogInFormComponent = () => {
+    let [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        rememberMe: false
+    })
+
+    const {email, password, rememberMe} = formData
+
+    const handleChange = (e) => setFormData(() => ({[e.target.name]: e.target.value}))
+
+    const loginUser = () => {
+        console.log(formData)
+    }
+
     return (
         <Container>
-            <form className="w-50 mx-auto">
+            <div className="w-50 mx-auto">
                 <h3>Login</h3>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input onChange={handleChange} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
 
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input onChange={handleChange} type="password" class="form-control" id="exampleInputPassword1" />
                     </div>
 
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <label class="form-check-label" for="exampleCheck1">Remember Me</label>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                    <button type="submit" class="btn btn-primary" onClick={loginUser}>Submit</button>
+                </div>
         </Container>
     )
 }
