be70fb770f82512517fc3ce42ecb0e24739c8751
josephkipkemoi <jkemboe@gmail.com>
2025-03-04 09:28:08 +0000

    Discussions page

---
+++ src/pages/Discussions.js
@@ -0,0 +1,15 @@
+import DiscussionsComponent from "../components/discussions"
+import FooterComponent from "../components/footer"
+import HeaderComponent from "../components/header"
+
+const Discussions = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <DiscussionsComponent/>
+            <FooterComponent/>
+        </div>
+    )
+}
+
+export default Discussions
