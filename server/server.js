245fa6e2aca87c6ea03f452ea14827e146009288
josephkipkemoi <jkemboe@gmail.com>
2025-03-04 13:33:10 +0000

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -18,14 +18,14 @@ app.use("/api/chatbot", require("./routes/api/chatbot"))
 app.use("/api/community", require("./routes/api/community"))
 
 // Serve static assets in production
-app.use(express.static("./build"))
+// app.use(express.static("./build"))
 
-if(process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "build", "index.html"))
-    })
-}
+// if(process.env.NODE_ENV === "production") {
+//     // Set static folder
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "build", "index.html"))
+//     })
+// }
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
